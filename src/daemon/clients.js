de3e6242cdfaf44a8358833238f2523f9b4000a0
b-fuze <b-fuze.root@live.com>
2018-06-08 06:17:25 -0400

    Use queue for loading tasks

--- src/daemon/clients.js
+++ src/daemon/clients.js
@@ -12,6 +12,7 @@ const path = __importStar(require("path"));
 const url_1 = require("url");
 const utils_1 = require("./utils");
 const facets_1 = require("./facets");
+const queue_1 = require("./queue");
 const tasks_1 = require("./tasks");
 const resolve_1 = require("./resolve");
 var Instruction;
@@ -30,57 +31,62 @@ exports.instructions = {
             if (provider && provider.validUrl(url, true)) {
                 const task = new tasks_1.Task(url, [], provider.facetId, provider.name);
                 done(null, task.id);
-                resolve_1.resolveProvider(url, (err, metadata) => {
-                    if (err) {
-                        console.error("ANV: Error loading metadata for task " + task.id);
-                    }
-                    else {
-                        task.title = metadata.title;
-                        task.cover = metadata.cover;
-                        task.dlDir = task.settings.dlPath + path.sep + tasks_1.getSimpleName(task.title);
-                        const fileNameBase = task.settings.minimalFileName ? tasks_1.getInitials(task.title) : tasks_1.getSimpleName(task.title);
-                        for (const source of metadata.sources) {
-                            let facet;
-                            const fileName = fileNameBase
-                                + (task.settings.numberPad
-                                    ? utils_1.getPadding(source.number, Math.max(Math.pow(10, Math.max(0, task.settings.padMinLength - 1)), metadata.sources.length))
-                                    : source.number)
-                                + "." + source.fileExtension;
-                            const media = new tasks_1.Media(source.number, fileName, task.list, task.id);
-                            task.list.push(media);
-                            switch (source.type) {
-                                case "mediasource":
-                                    facet = facets_1.getFacetByHost("provider", source.url);
-                                    media.sources.push(new tasks_1.MediaSourceDirect(source.url, facet.name, facet.facetId));
-                                    break;
-                                case "mirror":
-                                    facet = facets_1.getFacetByHost("mirror", source.url);
-                                    media.sources.push(new tasks_1.MediaSourceMirror(source.url, facet.name, facet.facetId));
-                                    break;
-                                case "stream":
-                                    facet = facets_1.getFacet("streamresolver", source.resolver);
-                                    media.sources.push(new tasks_1.MediaSourceStream(source.url, facet.name, facet.facetId));
-                                    break;
-                            }
+                queue_1.queueAdd("provider", provider.facetId, () => {
+                    resolve_1.resolveProvider(url, (err, metadata) => {
+                        if (err) {
+                            console.error("ANV: Error loading metadata for task " + task.id);
                         }
-                        // Create dl directory
-                        fs.stat(task.dlDir, (err, stats) => {
-                            if (err && err.code === 'ENOENT') {
-                                fs.mkdir(task.dlDir, (err) => {
-                                    if (!err) {
-                                        task.loaded = true;
-                                        task.triggerEvent("load", true);
-                                    }
-                                    else {
-                                        console.error("ANV: Error creating directory for task " + task.id, err);
-                                    }
-                                });
+                        else {
+                            task.title = metadata.title;
+                            task.cover = metadata.cover;
+                            task.dlDir = task.settings.dlPath + path.sep + tasks_1.getSimpleName(task.title);
+                            let fileNameBase = task.settings.minimalFileName ? tasks_1.getInitials(task.title) : tasks_1.getSimpleName(task.title);
+                            if (/^.+\d$/.test(fileNameBase)) {
+                                fileNameBase += "-";
                             }
-                            else {
-                                console.error("ANV: Directory for task already exists \"" + task.dlDir + "\"");
+                            for (const source of metadata.sources) {
+                                let facet;
+                                const fileName = fileNameBase
+                                    + (task.settings.numberPad
+                                        ? utils_1.getPadding(source.number, Math.max(Math.pow(10, Math.max(0, task.settings.padMinLength - 1)), metadata.sources.length))
+                                        : source.number)
+                                    + "." + source.fileExtension;
+                                const media = new tasks_1.Media(source.number, fileName, task.list, task.id);
+                                task.list.push(media);
+                                switch (source.type) {
+                                    case "mediasource":
+                                        facet = facets_1.getFacetByHost("provider", source.url);
+                                        media.sources.push(new tasks_1.MediaSourceDirect(source.url, facet.name, facet.facetId));
+                                        break;
+                                    case "mirror":
+                                        facet = facets_1.getFacetByHost("mirror", source.url);
+                                        media.sources.push(new tasks_1.MediaSourceMirror(source.url, facet.name, facet.facetId));
+                                        break;
+                                    case "stream":
+                                        facet = facets_1.getFacet("streamresolver", source.resolver);
+                                        media.sources.push(new tasks_1.MediaSourceStream(source.url, facet.name, facet.facetId));
+                                        break;
+                                }
                             }
-                        });
-                    }
+                            // Create dl directory
+                            fs.stat(task.dlDir, (err, stats) => {
+                                if (err && err.code === 'ENOENT') {
+                                    fs.mkdir(task.dlDir, (err) => {
+                                        if (!err) {
+                                            task.loaded = true;
+                                            task.triggerEvent("load", true);
+                                        }
+                                        else {
+                                            console.error("ANV: Error creating directory for task " + task.id, err);
+                                        }
+                                    });
+                                }
+                                else {
+                                    console.error("ANV: Directory for task already exists \"" + task.dlDir + "\"");
+                                }
+                            });
+                        }
+                    });
                 });
             }
             else {
--- src/daemon/clients.ts
+++ src/daemon/clients.ts
@@ -4,6 +4,7 @@ import {parse as parseUrl} from "url";
 import {getPadding} from "./utils";
 import {state} from "./state";
 import {getFacet, getFacetByHost} from "./facets";
+import {queueAdd} from "./queue";
 import {
   Task,
   Media,
@@ -34,58 +35,64 @@ export const instructions = {
         const task = new Task(url, [], provider.facetId, provider.name);
         done(null, task.id);
 
-        resolveProvider(url, (err, metadata) => {
-          if (err) {
-            console.error("ANV: Error loading metadata for task " + task.id);
-          } else {
-            task.title = metadata.title;
-            task.cover = metadata.cover;
+        queueAdd("provider", provider.facetId, () => {
+          resolveProvider(url, (err, metadata) => {
+            if (err) {
+              console.error("ANV: Error loading metadata for task " + task.id);
+            } else {
+              task.title = metadata.title;
+              task.cover = metadata.cover;
 
-            task.dlDir = task.settings.dlPath + path.sep + getSimpleName(task.title);
-            const fileNameBase = task.settings.minimalFileName ? getInitials(task.title) : getSimpleName(task.title);
+              task.dlDir = task.settings.dlPath + path.sep + getSimpleName(task.title);
+              let fileNameBase = task.settings.minimalFileName ? getInitials(task.title) : getSimpleName(task.title);
 
-            for (const source of metadata.sources) {
-              let facet;
-              const fileName = fileNameBase
-                             + (task.settings.numberPad
-                                ? getPadding(source.number, Math.max(Math.pow(10, Math.max(0, task.settings.padMinLength - 1)), metadata.sources.length))
-                                : source.number)
-                             + "." + source.fileExtension;
-              const media = new Media(source.number, fileName, task.list, task.id);
-              task.list.push(media);
-
-              switch (source.type) {
-                case "mediasource":
-                  facet = getFacetByHost("provider", source.url);
-                  media.sources.push(new MediaSourceDirect(source.url, facet.name, facet.facetId));
-                  break;
-                case "mirror":
-                  facet = getFacetByHost("mirror", source.url);
-                  media.sources.push(new MediaSourceMirror(source.url, facet.name, facet.facetId));
-                  break;
-                case "stream":
-                  facet = getFacet("streamresolver", source.resolver);
-                  media.sources.push(new MediaSourceStream(source.url, facet.name, facet.facetId));
-                  break;
+              if (/^.+\d$/.test(fileNameBase)) {
+                fileNameBase += "-";
               }
+
+              for (const source of metadata.sources) {
+                let facet;
+                const fileName = fileNameBase
+                               + (task.settings.numberPad
+                                  ? getPadding(source.number, Math.max(Math.pow(10, Math.max(0, task.settings.padMinLength - 1)), metadata.sources.length))
+                                  : source.number)
+                               + "." + source.fileExtension;
+                const media = new Media(source.number, fileName, task.list, task.id);
+                task.list.push(media);
+
+                switch (source.type) {
+                  case "mediasource":
+                    facet = getFacetByHost("provider", source.url);
+                    media.sources.push(new MediaSourceDirect(source.url, facet.name, facet.facetId));
+                    break;
+                  case "mirror":
+                    facet = getFacetByHost("mirror", source.url);
+                    media.sources.push(new MediaSourceMirror(source.url, facet.name, facet.facetId));
+                    break;
+                  case "stream":
+                    facet = getFacet("streamresolver", source.resolver);
+                    media.sources.push(new MediaSourceStream(source.url, facet.name, facet.facetId));
+                    break;
+                }
+              }
+
+              // Create dl directory
+              fs.stat(task.dlDir, (err, stats) => {
+                if (err && err.code === 'ENOENT') {
+                  fs.mkdir(task.dlDir, (err) => {
+                    if (!err) {
+                      task.loaded = true;
+                      task.triggerEvent("load", true);
+                    } else {
+                      console.error("ANV: Error creating directory for task " + task.id, err);
+                    }
+                  });
+                } else {
+                  console.error("ANV: Directory for task already exists \"" + task.dlDir + "\"");
+                }
+              });
             }
-
-            // Create dl directory
-            fs.stat(task.dlDir, (err, stats) => {
-              if (err && err.code === 'ENOENT') {
-                fs.mkdir(task.dlDir, (err) => {
-                  if (!err) {
-                    task.loaded = true;
-                    task.triggerEvent("load", true);
-                  } else {
-                    console.error("ANV: Error creating directory for task " + task.id, err);
-                  }
-                });
-              } else {
-                console.error("ANV: Directory for task already exists \"" + task.dlDir + "\"");
-              }
-            });
-          }
+          });
         });
       } else {
         done("No provider found for " + url, null);
