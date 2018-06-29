1675992538b588c17d233e9a584e723316d68319
b-fuze <b-fuze.root@live.com>
2018-06-28 21:22:25 -0400

    Add Gogoanime provider

---
+++ src/modules/providers/gogoanime.mod.js
@@ -0,0 +1,120 @@
+// @ts-check
+const {register, genericResolver} = require("anv");
+const parseUrl = require("url");
+
+const mirrors = {
+  "mp4upload": 1,
+  "yourupload": 1,
+};
+
+// @ts-ignore
+register("provider", {
+  name: "gogoanime",
+  displayName: "GOGO Anime",
+  description: "ANV GOGO Anime provider",
+  weight: 0,
+  delay: 650,
+  resolvers: {
+    mediaList: "cfdom",
+    mediaSource: "cfdom",
+  },
+  hosts: [
+    /^www\d*\.gogoanime\.se$/,
+  ],
+  validUrl(url, list) {
+    if (list) {
+      // https://www2.gogoanime.se/category/boku-no-hero-academia
+      return /^https?:\/\/www\d*\.gogoanime\.se\/+category\/+[a-zA-Z\d-]+\/*$/.test(url);
+    } else {
+      // https://www2.gogoanime.se/boku-no-hero-academia-episode-1
+      return /^https?:\/\/www\d*\.gogoanime\.se\/+[a-zA-Z\d-]+-episode(-\d+(\.\d+)?){1,2}$/.test(url);
+    }
+  },
+  tiers: [
+    ["vidstreaming", "vidstreaming"],
+  ],
+  mediaList(jSh) {
+    const title = jSh(".anime_info_body_bg h1")[0].textContent;
+    const cover = jSh(".anime_info_body_bg img")[0].src;
+    const episodeRanges = jSh("#episode_page").children;
+
+    const animeId = jSh("#movie_id").value;
+    const defaultEpisode = jSh("#default_ep").value;
+
+    const ranges = [];
+    let rangeIndex = 0;
+    const media = [];
+
+    for (const wrap of episodeRanges) {
+      const elm = wrap.children[0];
+      ranges.push([+elm.getAttribute("ep_start"), +elm.getAttribute("ep_end")]);
+    }
+
+    const rangeDelay = 1000;
+    void function getNextRange() {
+      const interval = setTimeout(function() {
+        const range = ranges[rangeIndex];
+        const urlBase = "https://" + parseUrl.parse(jSh.url).host;
+        const url = urlBase + "/load-list-episode?"
+                    + `ep_start=${ range[0] }&`
+                    + `ep_end=${ range[1] }&`
+                    + `id=${ animeId }&`
+                    + `default_ep=${ defaultEpisode }`;
+
+        genericResolver("cfdom", url, (err, jSh) => {
+          if (!err) {
+            const episodes = jSh("a").reverse();
+
+            for (const episode of episodes) {
+              media.push({
+                type: "mediasource",
+                url: urlBase + episode.href.trim(),
+                number: episode.children[0].textContent.match(/\d+/)[0],
+                fileExtension: "mp4",
+              });
+            }
+
+            // Go to next range (if any)
+            rangeIndex++;
+
+            if (rangeIndex === ranges.length) {
+              // Call done()
+              done({
+                title,
+                cover,
+                sources: media
+              });
+            } else {
+              getNextRange();
+            }
+          }
+        });
+      }, rangeDelay);
+    }();
+
+    let done;
+
+    // @ts-ignore
+    return new Promise((resolve, reject) => {
+      done = resolve;
+    });
+  },
+  mediaSource(jSh, direct) {
+    const mirrors = jSh(".anime_muti_link .anime > a");
+    const sources = [];
+
+    for (const mirror of mirrors) {
+      const name = mirror.childNodes[0].wholeText.trim().toLowerCase();
+
+      if (/vidstreaming/.test(name)) {
+        sources.push({
+          type: "mirror",
+          url: "https:" + mirror.getAttribute("data-video"),
+          tiers: [name],
+        });
+      }
+    }
+
+    return sources;
+  }
+});
